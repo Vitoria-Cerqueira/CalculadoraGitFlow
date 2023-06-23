c60a74f334e211ae16c8ce24a6132bf9f77ec118
laura.chagas <laura.chagas@zup.com.br>
2023-06-23 14:33:33 -0300

    Subindo o novo recurso dividirNumeros via feature/dividir

--- src/Calculator.java
+++ src/Calculator.java
@@ -4,7 +4,7 @@ public class Calculator {
     Scanner scan = new Scanner(System.in);
     public void menuCalc() {
         System.out.println("-----------------------------------------------------");
-        System.out.println("Escolha a operação que você deseja fazer: \n[1] SOMA " + "            " + "[2] SUBTRAIR\n" +  "         [3] SAIR");
+        System.out.println("Escolha a operação que você deseja fazer: \n[1] SOMA " + "            " + "[2] SUBTRAIR\n" +" [3] DIVIDIR" +  "         [4] SAIR");
         switch (scan.nextInt()) {
             case 1:
                 System.out.println("A soma dos números informados é igual a " + somarNumeros());
@@ -14,11 +14,11 @@ public class Calculator {
                 System.out.println("A subtração dos números informados é igual a " + subtrairNumeros());
                 menuCalc();
                 break;
-//            case 3:
-//                System.out.println("A divisão dos números informados é igual a " + dividirNumeros());
-//                menuCalc();
-//                break;
             case 3:
+                System.out.println("A divisão dos números informados é igual a " + dividirNumeros());
+                menuCalc();
+                break;
+            case 4:
                 System.out.println("Encerrando sistema...");
                 System.exit(0);
             default:
@@ -47,8 +47,17 @@ public class Calculator {
         n2 = scan.nextInt();
         return n1 - n2;
     }
-//    public int dividirNumeros(){
-//
-//        return 0;
-//    }
+    public int dividirNumeros(){
+
+        int n1, n2;
+
+        System.out.println("Informe um número: ");
+        n1 = scan.nextInt();
+
+        System.out.println("Informe um número: ");
+        n2 = scan.nextInt();
+
+        return n1 / n2;
+
+    }
 }
